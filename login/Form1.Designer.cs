67361238a63641ff2440bdb100440f1aa83f51bd
josephtjoshy <josephtjoshy9@gmail.com>
2020-08-27 17:37:00 +0530

    start

--- login/Form1.Designer.cs
+++ login/Form1.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipaddressinput = new System.Windows.Forms.TextBox();
+            this.portinput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectbutton = new System.Windows.Forms.Button();
+            this.sendtext = new System.Windows.Forms.TextBox();
+            this.reciveText = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.connecting = new System.Windows.Forms.Label();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.startlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label1.Location = new System.Drawing.Point(115, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ip address";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ipaddressinput
+            // 
+            this.ipaddressinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.ipaddressinput.Location = new System.Drawing.Point(278, 90);
+            this.ipaddressinput.Name = "ipaddressinput";
+            this.ipaddressinput.Size = new System.Drawing.Size(171, 23);
+            this.ipaddressinput.TabIndex = 1;
+            this.ipaddressinput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // portinput
+            // 
+            this.portinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.portinput.Location = new System.Drawing.Point(278, 138);
+            this.portinput.Name = "portinput";
+            this.portinput.Size = new System.Drawing.Size(171, 23);
+            this.portinput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(115, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "port";
+            // 
+            // connectbutton
+            // 
+            this.connectbutton.Location = new System.Drawing.Point(323, 201);
+            this.connectbutton.Name = "connectbutton";
+            this.connectbutton.Size = new System.Drawing.Size(75, 23);
+            this.connectbutton.TabIndex = 4;
+            this.connectbutton.Text = "connect";
+            this.connectbutton.UseVisualStyleBackColor = true;
+            this.connectbutton.Click += new System.EventHandler(this.connectbutton_Click);
+            // 
+            // sendtext
+            // 
+            this.sendtext.Location = new System.Drawing.Point(578, 66);
+            this.sendtext.Name = "sendtext";
+            this.sendtext.Size = new System.Drawing.Size(185, 20);
+            this.sendtext.TabIndex = 5;
+            // 
+            // reciveText
+            // 
+            this.reciveText.Location = new System.Drawing.Point(587, 118);
+            this.reciveText.Multiline = true;
+            this.reciveText.Name = "reciveText";
+            this.reciveText.Size = new System.Drawing.Size(175, 132);
+            this.reciveText.TabIndex = 6;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(779, 66);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 7;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            // 
+            // connecting
+            // 
+            this.connecting.AutoSize = true;
+            this.connecting.Location = new System.Drawing.Point(320, 280);
+            this.connecting.Name = "connecting";
+            this.connecting.Size = new System.Drawing.Size(74, 13);
+            this.connecting.TabIndex = 8;
+            this.connecting.Text = "press connect";
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(587, 280);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 9;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.Location = new System.Drawing.Point(687, 280);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.stopbutton.TabIndex = 10;
+            this.stopbutton.Text = "stop";
+            this.stopbutton.UseVisualStyleBackColor = true;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // startlabel
+            // 
+            this.startlabel.AutoSize = true;
+            this.startlabel.Location = new System.Drawing.Point(638, 318);
+            this.startlabel.Name = "startlabel";
+            this.startlabel.Size = new System.Drawing.Size(55, 13);
+            this.startlabel.TabIndex = 11;
+            this.startlabel.Text = "press start";
+            this.startlabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(913, 340);
+            this.Controls.Add(this.startlabel);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.connecting);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.reciveText);
+            this.Controls.Add(this.sendtext);
+            this.Controls.Add(this.connectbutton);
+            this.Controls.Add(this.portinput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ipaddressinput);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipaddressinput;
+        private System.Windows.Forms.TextBox portinput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectbutton;
+        private System.Windows.Forms.TextBox sendtext;
+        private System.Windows.Forms.TextBox reciveText;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label connecting;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.Label startlabel;
     }
 }
 
--- login/Form1.cs
+++ login/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace login
 {
@@ -15,6 +17,62 @@ namespace login
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectbutton_Click(object sender, EventArgs e)
+        {
+            String ip = ipaddressinput.Text;
+            String port = portinput.Text;
+            if(ip.Length<1 && port.Length<1)
+            {
+                connecting.Text = "type an ip address and port";
+            }
+            else if(port.Length<1)
+            {
+                connecting.Text = "type a port number";
+            }
+            else if(ip.Length<1)
+            {
+                connecting.Text = "type an ip address";
+            }
+            else
+            {
+                connecting.Text = "Conecting......";
+            }
+        }
+        TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 4500);
+        private void startbutton_Click(object sender, EventArgs e)
+        {            
+            listener.Start();
+            startlabel.Text = "Initiated";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            listener.Stop();
+            startlabel.Text = "Terminated";
         }
     }
 }
